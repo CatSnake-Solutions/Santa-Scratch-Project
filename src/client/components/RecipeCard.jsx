fbcdedc49f3b2674f0a1b71a0d7c4dbfe00ffc97
nicholassuzuki <nicholassuzuki@yahoo.com>
2021-11-26 11:32:00 -0500

    fetch request added to recipe card and recipe now displays

--- src/client/components/RecipeCard.jsx
+++ src/client/components/RecipeCard.jsx
@@ -1,16 +1,33 @@
 import React from "react";
+import { useState } from 'react';
+import DetailsList from './DetailsList.jsx';
 
 const RecipeCard = (props) => {
+	const [details, setDetails] = useState(null);
+
+	// getRecipeDetails - dispatch fetch request for specific recipe 
+	function getRecipeDetails(id) {
+		console.log(`getRecipesDetails: ${id}`)
+		// console.log("https://api.spoonacular.com/recipes/`${id}`/analyzedInstructions?apiKey=c3438c093ba3428dbb48800c1f2d8c23")
+		console.log(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=c3438c093ba3428dbb48800c1f2d8c23`)
+		// Make fetch request
+		fetch(`https://api.spoonacular.com/recipes/${id}/analyzedInstructions?apiKey=c3438c093ba3428dbb48800c1f2d8c23`)
+			.then(data => data.json())
+			.then(data => {
+				console.log(data);
+				setDetails(data[0].steps)
+			})
+			.catch(err => console.log(err));
+	}
+
 	return (
 		<div className='recipeContainer'>
 			<div key={"RecipeCard:" + props.id}>
 				<h1 className='title'>{props.title}</h1>
 				<img src={props.image}></img>
-				<ul className='recipeDetailsList'>
-					{/*   <li>Prep Time: {props.readyInMinutes}</li> */}
-					{/*<li>Serving Size: {props.servings}</li> */}
-					<a href={props.sourceUrl}>Link to recipe!</a>
-				</ul>
+
+				<button onClick={() => getRecipeDetails(props.id)}>View Recipe</button>
+				{details && <DetailsList list={details} /> }
 			</div>
 		</div>
 	);
